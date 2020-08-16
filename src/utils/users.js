85921562eae3790bf9ed2502b8cb13f21f8612f5
ajayriq <ajay@responseiq.com>
2020-08-16 19:53:07 +0530

    init commit

---
+++ src/utils/users.js
@@ -0,0 +1,57 @@
+const users = []
+
+// addUser, removeUser, getUser, getUsersInRooms
+
+const addUser = ({ id, username, room }) => {
+  // clean data
+  username = username.trim().toLowerCase()
+  room = room.trim().toLowerCase()
+
+  // Validate the data
+  if (!username || !room) {
+    return {
+      error: 'Username and room are required',
+    }
+  }
+
+  // check for existing user
+  const existingUser = users.find((user) => {
+    return user.room === room && user.username === username
+  })
+
+  // validate username
+  if (existingUser) {
+    return {
+      error: 'Username is taken',
+    }
+  }
+
+  // store user
+  const user = { id, username, room }
+  users.push(user)
+  return { user }
+}
+
+const removeUser = (id) => {
+  const index = users.findIndex((user) => user.id === id)
+
+  if (index !== -1) {
+    return users.splice(index, 1)[0]
+  }
+}
+
+const getUser = (id) => {
+  return users.find((user) => user.id === id)
+}
+
+const getUsersInRoom = (room) => {
+  room = room.trim().toLowerCase()
+  return users.filter((user) => user.room === room)
+}
+
+module.exports = {
+  addUser,
+  removeUser,
+  getUser,
+  getUsersInRoom,
+}
